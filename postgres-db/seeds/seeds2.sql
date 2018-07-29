f47fe9a970662d9ad06145a6756f536d99f08675
Spenser Holstein <spenser.holstein@gmail.com>
2018-07-29 11:10:24 -0400

    removed redundant seed file and deleted enrollments

--- postgres-db/seeds/seeds2.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO student2s (first_name, last_name, grade, email, createdAt, updatedAt) VALUES
-  ('Luke', 'Skywalker', 4, 'luke@mail.com', current_timestamp, current_timestamp),
-  ('Darth', 'Vader', 4, 'anakin@mail.com', current_timestamp, current_timestamp),
-  ('Liea', 'Organa', 4, 'liea@mail.com', current_timestamp , current_timestamp),
-  ('Han', 'Solo', 4, 'han@mail.com', current_timestamp, current_timestamp),
-  ('Lando', 'Calrissian', 4, 'lando@mail.com', current_timestamp, current_timestamp),
-  ('Obi-Wan', 'Kenobi', 4, 'obi@mail.com', current_timestamp, current_timestamp);
